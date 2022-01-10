771a657eeeb879caff7867dc5fe057eb97c120bf
JokingJester <81639209+JokingJester@users.noreply.github.com>
2022-01-09 18:16:37 -0600

    Created Enemy Script
    This script chooses a random playable asset and plays it. When the assets duration is over, it plays a different random one.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Enemy : MonoBehaviour
+{
+    private double _duration;
+
+    [SerializeField] private PlayableDirector _director;
+    [SerializeField] private PlayableAsset[] _assets;
+    void Start()
+    {
+        ChooseRandomPattern();
+    }
+
+    private void ChooseRandomPattern()
+    {
+        int randomNumb = Random.Range(0, _assets.Length - 1);
+        _director.playableAsset = _assets[randomNumb];
+        _duration = Time.time + _director.playableAsset.duration;
+        _director.Play();
+    }
+
+    void Update()
+    {
+        if(Time.time > _duration)
+        {
+            ChooseRandomPattern();
+        }
+    }
+}
