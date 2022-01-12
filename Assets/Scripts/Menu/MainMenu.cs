59b0debd8f3e14a5ebe6e8152d342a856e742258
JokingJester <81639209+JokingJester@users.noreply.github.com>
2022-01-12 00:11:29 -0600

    Added Sliders To Option Menu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-
+    [SerializeField] private Slider _brightnessSlider;
     public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -13,5 +14,10 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void AdjustBrightness()
+    {
+       //effect directional light
     }
 }
